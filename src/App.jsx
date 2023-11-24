dcfada4cb738a017c41cb24e94f14619a081f041
RoshanKhatri0 <lastkhatri@gmail.com>
2023-11-24 14:28:03 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Test from './Test'
+import { Demo } from './Demo'
+
+
+const App = () => {
+  return (
+    <div className='App'>
+        <Test/>
+        <Demo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <>
+    <div className='bg-primary'>This is react page.</div>
+    </>
+    
+  )
+}
+
+export default Test
